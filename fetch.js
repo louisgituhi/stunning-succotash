743230ecfff82c5f64f8e0e6373607ce65c8b3ff
louis gituhi <louisgituhi@gamil.com>
2023-11-04 12:25:38 +0300

    added comprehensive error handling

--- fetch.js
+++ fetch.js
@@ -1,21 +1,40 @@
 // fetch data from a url => https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
 
-fetch('https://jsonplaceholder.typicode.com/users')
 
-// .then triggers when the server responds
+try {
 
-.then(function(response) {
+    fetch('https://jsonplaceholder.typicode.com/users//3')
+    
+    // .then triggers when the server responds
 
-    // when we get the full json response
-    return response.json();
-})
+    .then(function(response) {
+        if (response.status === 404) {
+            throw new Error('Url not found');
+        } else if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        // when we get the full json response
+        return response.json();
 
-
-// lets log the names of users to the console
-// since the url returns an array, we can loop over it
-
-.then(function(userData) {
-    userData.forEach(function(user) {
-        console.log(user.name);
     })
-})
+    
+    
+    // lets log the names of users to the console
+    // since the url returns an array, we can loop over it
+    
+    .then(function(userData) {
+        userData.forEach(function(user) {
+            console.log(user.name);
+        })
+    })
+    .catch(function(err) {
+        console.error('fetch error', err.name);
+        console.error('fetch error', err.message);
+    })
+
+
+} catch (e) {
+    console.error('general: ', e.message);
+    console.error('general: ', e.name);
+}
